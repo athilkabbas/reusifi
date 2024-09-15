fbe9a92a75d1468662b3034bf29e0ef47e6e7068
Athil <a.kuzhikkandathil@accenture.com>
2024-09-15 17:30:03 +0100

    change sign in uri

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -16,8 +16,8 @@ const awsmobile = {
             "profile",
             "aws.cognito.signin.user.admin"
         ],
-        "redirectSignIn": "http://localhost:3000/,https://reusifi.com/",
-        "redirectSignOut": "http://localhost:3000/,https://reusifi.com/,http://localhost:3000/",
+        "redirectSignIn": "https://reusifi.com/,https://reusifi.com/",
+        "redirectSignOut": "https://reusifi.com/,https://reusifi.com/,https://reusifi.com/",
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
