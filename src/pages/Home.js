1932d7cf7f1e4872e30b6623de18062dceabb6b0
Athil <athilkabbas@gmail.com>
2025-07-09 01:51:40 +0100

    fixed data sorting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -337,7 +337,7 @@ useEffect(() => {
     setExhaustedShards({})
     setData([]);
     setInitialLoad(true);
-    setPriceFilter(event.target.value);
+    setPriceFilter(event.target.value === 'true' ? 'LOWTOHIGH' : 'HIGHTOLOW');
   };
   return (
     <Layout style={{ height: "100vh", overflow: "hidden", background:"#F9FAFB" }}>
