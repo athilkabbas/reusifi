c4220f655bc421f12188cefd40fd4b51a929a5fa
Athil <athilkabbas@gmail.com>
2025-08-04 19:13:19 +0100

    fix login

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -1,11 +1,11 @@
 const awsmobile = {
   aws_project_region: "ap-south-1",
   aws_cognito_region: "ap-south-1",
-  aws_user_pools_id: "ap-south-1_R9wR17tT5",
-  aws_user_pools_web_client_id: "7jeltl9uu4apdfugbmo1cub7ti",
+  aws_user_pools_id: "ap-south-1_8TOOXlT5Y",
+  aws_user_pools_web_client_id: "q57qj9r5ehaoli43r10jic2rp",
   oauth: {
     domain: "auth.reusifi.com",
-    scope: ["openid", "email", "profile", "phone"],
+    scope: ["openid", "email", "phone"],
     redirectSignIn:
       "https://www.reusifi.com/,https://reusifi.com/,http://localhost:3000/",
     redirectSignOut:
