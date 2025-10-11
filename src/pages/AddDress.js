b31f99d5231b38d4eb400f4c7e2ca3cc7570fac7
Athil <athilkabbas@gmail.com>
2025-10-11 16:03:52 +0100

    fix scroll pop up

--- src/pages/AddDress.js
+++ src/pages/AddDress.js
@@ -387,11 +387,16 @@ const AddDress = () => {
   const bottomRef = useRef(null);
 
   const scrollToBottom = () => {
-    requestAnimationFrame(() => {
-      if (bottomRef?.current) {
-        bottomRef.current?.scrollIntoView({ behavior: "auto", block: "end" });
-      }
-    });
+    setTimeout(() => {
+      requestAnimationFrame(() => {
+        if (bottomRef?.current) {
+          bottomRef.current.scrollIntoView({
+            behavior: "smooth",
+            block: "end",
+          });
+        }
+      });
+    }, 100);
   };
   return (
     <Layout
