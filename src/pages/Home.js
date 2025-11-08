ad6db56656f755c588c39bbf422a1c071fce52a5
Athil <athilkabbas@gmail.com>
2025-11-08 16:45:01 +0000

    improve scroll

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -643,8 +643,8 @@ const Home = () => {
         closable={{ "aria-label": "Close Button" }}
         onClose={onClose}
         open={drawerOpen}
-        height={"100%"}
-        width={"100%"}
+        height={"100dvh"}
+        width={"100dvw"}
       >
         <Space
           size="middle"
