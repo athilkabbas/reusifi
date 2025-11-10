81b4685f10d2e9660ea9d18d40488163f32e6a96
Athil <athilkabbas@gmail.com>
2025-11-10 21:44:15 +0000

    prevent touch propagation

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -811,6 +811,9 @@ const Chat = () => {
                 scrollToBottom();
                 setOpen(true);
               }}
+              onTouchMove={() => {
+                setOpen(true);
+              }}
               autoSize={{ minRows: 1, maxRows: 5 }}
               onChange={(event) => handleChange(event.target.value)}
               placeholder="Enter message"
